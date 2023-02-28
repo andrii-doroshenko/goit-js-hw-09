6a73bb0af4f49fc18cb4c60260df6ccae44fa43f
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-02-28 19:41:29 +0200

    feature bgChanging

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', onStartChangeColor);
+refs.stopBtn.addEventListener('click', onStopChangeColor);
+
+function onStartChangeColor() {
+  intervalId = setInterval(() => {
+    changeColorEl(refs.body);
+  }, 1000);
+
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function onStopChangeColor() {
+  clearInterval(intervalId);
+
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
+
+function changeColorEl(el) {
+  el.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

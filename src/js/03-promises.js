9cce6bfb9d58aaf749026716199392d011485632
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-03-05 12:36:13 +0200

    feature promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('form');
+const amountInput = document.querySelector('input[name="amount"]');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+
+formEl.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  for (let i = 0; i < amountInput.value; i += 1) {
+    createPromise(i + 1, Number(delayInput.value) + Number(stepInput.value) * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          { useIcon: false }
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          { useIcon: false }
+        );
+      });
   }
 }
